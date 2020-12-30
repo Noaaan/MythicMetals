00ab67c0ec709cc5fdc99060a60a01bd8a76748e
Nikola Schrodinger <desuhansa@gmail.com>
2020-12-30 19:18:38 +0100

    Added anvils

---
+++ src/main/java/nourl/mythicmetals/mixin/AnvilScreenHandlerMixin.java
@@ -0,0 +1,22 @@
+package nourl.mythicmetals.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.screen.AnvilScreenHandler;
+import nourl.mythicmetals.blocks.MythicMetalsAnvils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AnvilScreenHandler.class)
+public class AnvilScreenHandlerMixin {
+
+    @Inject(method = "canUse", at = @At("HEAD"), cancellable = true)
+    public void canUse(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        if (MythicMetalsAnvils.ANVILS.contains(state.getBlock())) {
+            cir.setReturnValue(true);
+            cir.cancel();
+        }
+    }
+
+}
