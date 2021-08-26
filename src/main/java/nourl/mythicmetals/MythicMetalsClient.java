ea029dd7fc68b6dd46cca14726fc26db86cba7cb
Noaaan <noaaan@hotmail.com>
2021-08-26 23:34:16 +0200

    Registry refactors

--- src/main/java/nourl/mythicmetals/MythicMetalsClient.java
+++ src/main/java/nourl/mythicmetals/MythicMetalsClient.java
@@ -2,24 +2,16 @@ package nourl.mythicmetals;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-//import net.fabricmc.fabric.api.client.model.ModelProviderContext;
-//import net.fabricmc.fabric.api.client.rendering.v1.ArmorRenderingRegistry;
 import net.minecraft.client.render.RenderLayer;
-//import net.minecraft.util.Identifier;
 import nourl.mythicmetals.blocks.MythicBlocks;
-//import nourl.mythicmetals.armor.HallowedArmor;
-//import nourl.mythicmetals.armor.MetallurgiumArmor;
-//import nourl.mythicmetals.registry.RegisterArmor;
 
 public class MythicMetalsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         makeOpaque();
-        //renderHallowedArmorModel();
-        //renderMetallurgiumArmorModel();
     }
 
-    // Makes custom model blocks see trough, like chains
+    // Makes custom model blocks see through, like chains
     public void makeOpaque() {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutoutMipped(),
                 MythicBlocks.ADAMANTITE_CHAIN,
@@ -52,23 +44,5 @@ public class MythicMetalsClient implements ClientModInitializer {
                 MythicBlocks.PALLADIUM_CHAIN
         );
     }
-    // Adds custom entity models to hallowed and metallurgium armor, currently deprecated
-//    public void renderHallowedArmorModel() {
-//        ArmorRenderingRegistry.ModelProvider modelProvider = (entity, stack, slot, original) -> ((HallowedArmor) stack.getItem()).getArmorModel(entity, stack, slot, original);
-//        ArmorRenderingRegistry.TextureProvider textureProvider = (entity, stack, slot, secondLayer, suffix, original) -> new Identifier(((HallowedArmor) stack.getItem()).getArmorTexture(stack, slot));
-//        ArmorRenderingRegistry.registerModel(modelProvider, RegisterArmor.HALLOWED_HELMET);
-//        ArmorRenderingRegistry.registerTexture(textureProvider, RegisterArmor.HALLOWED_HELMET);
-//        ArmorRenderingRegistry.registerModel(modelProvider, RegisterArmor.HALLOWED_CHESTPLATE);
-//        ArmorRenderingRegistry.registerTexture(textureProvider, RegisterArmor.HALLOWED_CHESTPLATE);
-//        ArmorRenderingRegistry.registerModel(modelProvider, RegisterArmor.HALLOWED_LEGGINGS);
-//        ArmorRenderingRegistry.registerTexture(textureProvider, RegisterArmor.HALLOWED_LEGGINGS);
-//    }
-//    ModelProviderContext
-//    public void renderMetallurgiumArmorModel() {
-//        ArmorRenderingRegistry.ModelProvider modelProvider = (entity, stack, slot, original) -> ((MetallurgiumArmor) stack.getItem()).getArmorModel(entity, stack, slot, original);
-//        ArmorRenderingRegistry.TextureProvider textureProvider = (entity, stack, slot, secondLayer, suffix, original) -> new Identifier(((MetallurgiumArmor) stack.getItem()).getArmorTexture(stack, slot));
-//        ArmorRenderingRegistry.registerModel(modelProvider, RegisterArmor.METALLURGIUM_HELMET);
-//        ArmorRenderingRegistry.registerTexture(textureProvider, RegisterArmor.METALLURGIUM_HELMET);
-//    }
 
 }
--- src/main/java/nourl/mythicmetals/registry/RegistryHelper.java
+++ src/main/java/nourl/mythicmetals/registry/RegistryHelper.java
@@ -8,34 +8,38 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import nourl.mythicmetals.MythicMetals;
 
-// Contains helper methods for registering items and blocks
+// Contains helper methods for registering items and blocks.
+// Note that it is not used for creating new items or blocks, only registering.
+// Written by Noaaan
 public class RegistryHelper {
 
     public static Identifier id(String path) {
         return new Identifier(MythicMetals.MOD_ID, path);
     }
 
-    public static void registerItem(String path, Item item) {
+    public static void item(String path, Item item) {
         Registry.register(Registry.ITEM, id(path), item);
     }
 
-    public static void registerBlock(String path, Block block) {
-        Registry.register(Registry.BLOCK, new Identifier(MythicMetals.MOD_ID, path), block);
+    public static void block(String path, Block block, ItemGroup group) {
+        Registry.register(Registry.BLOCK, id(path), block);
+        Registry.register(Registry.ITEM, id(path), new BlockItem(block, new Item.Settings().group(group)));
     }
 
-    public static void registerBlockItem(String path, Block block, ItemGroup group) {
-        Registry.register(Registry.ITEM, new Identifier(MythicMetals.MOD_ID, path), new BlockItem(block, new Item.Settings().group(group)));
+    public static void block(String path, Block block, ItemGroup group, boolean fireproof) {
+        if (fireproof) {
+            Registry.register(Registry.BLOCK, id(path), block);
+            Registry.register(Registry.ITEM, id(path), new BlockItem(block, new Item.Settings().group(group).fireproof()));
+        } else {
+            block(path, block, group);
+        }
     }
 
-    public static void registerFireproofBlockItem(String path, Block block, ItemGroup group) {
-        Registry.register(Registry.ITEM, new Identifier(MythicMetals.MOD_ID, path), new BlockItem(block, new Item.Settings().group(group).fireproof()));
-    }
-
-    public static void registerChainBlock(String path, Block block) {
+    public static void chain(String path, Block block) {
         Registry.register(Registry.BLOCK, new Identifier(MythicMetals.CHAIN_ID, path), block);
     }
 
-    public static void registerChainItem(String path, Block block) {
+    public static void chainItem(String path, Block block) {
         Registry.register(Registry.ITEM, new Identifier(MythicMetals.CHAIN_ID, path), new BlockItem(block, new Item.Settings().group(MythicMetals.MYTHICMETALS_DECOR)));
     }
 }
