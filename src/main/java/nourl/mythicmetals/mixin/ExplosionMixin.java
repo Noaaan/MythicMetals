ae35347f65831dffe4ccbab96833719d935c9226
Noaaan <noaaan@hotmail.com>
2022-03-05 17:46:45 +0100

    Fixed Smite ability not applying correctly, made Banglum TNT respect who detonated it
    Signed-off-by: Noaaan <noaaan@hotmail.com>

---
+++ src/main/java/nourl/mythicmetals/mixin/ExplosionMixin.java
@@ -0,0 +1,26 @@
+package nourl.mythicmetals.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.explosion.Explosion;
+import nourl.mythicmetals.blocks.BanglumTntEntity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Explosion.class)
+public class ExplosionMixin {
+
+    @Shadow @Final private @Nullable Entity entity;
+
+    @Inject(method = "getCausingEntity", at = @At("HEAD"), cancellable = true)
+    public void superBang(CallbackInfoReturnable<LivingEntity> cir) {
+        if (this.entity != null && this.entity instanceof BanglumTntEntity){
+            cir.setReturnValue(((BanglumTntEntity) this.entity).getCausingEntity());
+        }
+    }
+}
