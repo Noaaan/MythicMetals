562c5cc8738e6bc1f64c2a5d510f14d63ac5d26f
Noaaan <noaaan@hotmail.com>
2022-08-06 18:49:02 +0200

    Nerfed Osmium, and Buffed Star Platinum, mining levels
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/nourl/mythicmetals/tools/ToolMaterials.java
+++ src/main/java/nourl/mythicmetals/tools/ToolMaterials.java
@@ -28,12 +28,12 @@ public enum ToolMaterials implements ToolMaterial {
     MIDAS_GOLD(3, 300, 13.0F, 3.0F, 30, () -> Ingredient.ofItems(MythicItems.Ingots.MIDAS_GOLD_INGOT)),
     MYTHRIL(4, 1445, 13.0F, 3.0F, 22, () -> Ingredient.ofItems(MythicItems.Ingots.MYTHRIL_INGOT)),
     ORICHALCUM(4, 2048, 6.0F, 4.0F, 16, () -> Ingredient.ofItems(MythicItems.Ingots.ORICHALCUM_INGOT)),
-    OSMIUM(3, 584, 7.0F, 2.0F, 13, () -> Ingredient.ofItems(MythicItems.Ingots.OSMIUM_INGOT)),
+    OSMIUM(2, 584, 7.0F, 2.0F, 13, () -> Ingredient.ofItems(MythicItems.Ingots.OSMIUM_INGOT)),
     PALLADIUM(4, 1234, 8.0F, 3.5F, 16, () -> Ingredient.ofItems(MythicItems.Ingots.PALLADIUM_INGOT)),
     PROMETHEUM(3, 1472, 6.0F, 4.0F, 15, () -> Ingredient.ofItems(MythicItems.Ingots.PROMETHEUM_INGOT)),
     QUADRILLUM(2, 321, 5.0F, 2.5F, 8, () -> Ingredient.ofItems(MythicItems.Ingots.QUADRILLUM_INGOT)),
     RUNITE(3, 1337, 8.9F, 3.3F, 17, () -> Ingredient.ofItems(MythicItems.Ingots.RUNITE_INGOT)),
-    STAR_PLATINUM(3, 1300, 9.0F, 4.0F, 18, () -> Ingredient.ofItems(MythicItems.Ingots.STAR_PLATINUM)),
+    STAR_PLATINUM(4, 1300, 9.0F, 4.0F, 18, () -> Ingredient.ofItems(MythicItems.Ingots.STAR_PLATINUM)),
     STEEL(2, 600, 6.5F, 2.5F, 12, () -> Ingredient.ofItems(MythicItems.Ingots.STEEL_INGOT)),
     STORMYX(3, 1305, 8.0F, 3.5F, 20, () -> Ingredient.ofItems(MythicItems.Ingots.STORMYX_INGOT));
 
