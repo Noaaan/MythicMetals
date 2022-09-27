f301793774e06ff0b328c65d8c57e11e9ff98253
Noaaan <noaaan@hotmail.com>
2022-09-27 10:03:32 +0200

    Mining Speed buff
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/nourl/mythicmetals/tools/MythicToolMaterials.java
+++ src/main/java/nourl/mythicmetals/tools/MythicToolMaterials.java
@@ -17,7 +17,7 @@ public enum MythicToolMaterials implements ToolMaterial {
     BANGLUM(2, 260, 11.0F, 2.0F, 1, () -> Ingredient.ofItems(MythicItems.Ingots.BANGLUM_INGOT)),
     BRONZE(2, 354, 5.5F, 2.5F, 14, () -> Ingredient.ofItems(MythicItems.Ingots.BRONZE_INGOT)),
     CARMOT(3, 1130, 11.5F, 3.0F, 42, () -> Ingredient.ofItems(MythicItems.Ingots.CARMOT_INGOT)),
-    CELESTIUM(5, 2270, 16.9F, 6.0F, 26, () -> Ingredient.ofItems(MythicItems.Ingots.CELESTIUM_INGOT)),
+    CELESTIUM(5, 2270, 20.0F, 6.0F, 26, () -> Ingredient.ofItems(MythicItems.Ingots.CELESTIUM_INGOT)),
     COPPER(1, 125, 4.5F, 1.0F, 8, () -> Ingredient.ofItems(Items.COPPER_INGOT)),
     DURASTEEL(3, 800, 7.1F, 3.5F, 12, () -> Ingredient.ofItems(MythicItems.Ingots.DURASTEEL_INGOT)),
     GILDED_MIDAS_GOLD(3, 999, 13.0F, 4.0F, 30, () -> Ingredient.ofItems(MythicBlocks.MIDAS_GOLD.getStorageBlock().asItem())),
@@ -26,7 +26,7 @@ public enum MythicToolMaterials implements ToolMaterial {
     LEGENDARY_BANGLUM(3, 1040, 12.0F, 4.0F, 2, () -> Ingredient.ofItems(MythicItems.BANGLUM_CHUNK)),
     METALLURGIUM(5, 3000, 15.0F, 8.0F, 30, () -> Ingredient.ofItems(MythicItems.Ingots.METALLURGIUM_INGOT)),
     MIDAS_GOLD(3, 300, 13.0F, 3.0F, 30, () -> Ingredient.ofItems(MythicItems.Ingots.MIDAS_GOLD_INGOT)),
-    MYTHRIL(4, 1564, 13.0F, 3.0F, 22, () -> Ingredient.ofItems(MythicItems.Ingots.MYTHRIL_INGOT)),
+    MYTHRIL(4, 1564, 14.3F, 3.0F, 22, () -> Ingredient.ofItems(MythicItems.Ingots.MYTHRIL_INGOT)),
     ORICHALCUM(4, 2048, 6.0F, 4.0F, 16, () -> Ingredient.ofItems(MythicItems.Ingots.ORICHALCUM_INGOT)),
     OSMIUM(2, 584, 7.0F, 2.0F, 13, () -> Ingredient.ofItems(MythicItems.Ingots.OSMIUM_INGOT)),
     PALLADIUM(4, 1234, 8.0F, 3.5F, 16, () -> Ingredient.ofItems(MythicItems.Ingots.PALLADIUM_INGOT)),
@@ -35,7 +35,7 @@ public enum MythicToolMaterials implements ToolMaterial {
     RUNITE(3, 1337, 8.9F, 3.3F, 17, () -> Ingredient.ofItems(MythicItems.Ingots.RUNITE_INGOT)),
     STAR_PLATINUM(4, 1300, 9.0F, 4.0F, 18, () -> Ingredient.ofItems(MythicItems.Ingots.STAR_PLATINUM)),
     STEEL(2, 600, 6.5F, 2.5F, 12, () -> Ingredient.ofItems(MythicItems.Ingots.STEEL_INGOT)),
-    STORMYX(3, 1305, 8.0F, 3.5F, 20, () -> Ingredient.ofItems(MythicItems.Ingots.STORMYX_INGOT));
+    STORMYX(3, 1305, 8.5F, 3.5F, 20, () -> Ingredient.ofItems(MythicItems.Ingots.STORMYX_INGOT));
 
     private final int miningLevel;
     private final int itemDurability;
