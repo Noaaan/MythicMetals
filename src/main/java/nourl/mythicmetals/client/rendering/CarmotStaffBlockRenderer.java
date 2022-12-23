953d30fcdcf0181ff2a8c31b0af74c4a611c642f
Noaaan <noaaan@hotmail.com>
2022-12-23 12:44:43 +0100

    Re-factor CarmotStaffBlockRenderer
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/nourl/mythicmetals/client/rendering/CarmotStaffBlockRenderer.java
+++ src/main/java/nourl/mythicmetals/client/rendering/CarmotStaffBlockRenderer.java
@@ -27,67 +27,83 @@ import java.util.function.Consumer;
 public class CarmotStaffBlockRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer, ExtraModelProvider {
     public static final ModelIdentifier CARMOT_STAFF_ID = new ModelIdentifier(RegistryHelper.id("carmot_staff_base"), "inventory");
     private static final Identifier WORLD_BORDER = new Identifier("textures/misc/forcefield.png");
+
     @Override
     public void render(ItemStack staff, ModelTransformation.Mode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int light, int overlay) {
         var block = staff.has(CarmotStaff.STORED_BLOCK) ? (staff.get(CarmotStaff.STORED_BLOCK)) : Blocks.AIR;
         var instance = MinecraftClient.getInstance();
-        var model = instance.getBakedModelManager().getModel(CARMOT_STAFF_ID);
+        var staffModel = instance.getBakedModelManager().getModel(CARMOT_STAFF_ID);
         var blockModel = instance.getBakedModelManager().getBlockModels().getModel(block.getDefaultState());
 
         boolean shouldRenderRainbowShield = CarmotStaff.hasBlockInStaff(staff, MythicBlocks.STORMYX.getStorageBlock());
 
         if (shouldRenderRainbowShield && mode.isFirstPerson() && staff.get(CarmotStaff.IS_USED)) {
-            matrices.push();
-            // Rainbow Handling
-            double delta = System.currentTimeMillis() / 45.0;
-
-            double hue = delta % 360.0;
-            float saturation = 1;
-            float constantvalue = 1;
-
-            int color = MathHelper.hsvToRgb((float) (hue / 360), saturation, constantvalue);
-
-            float[] rgbColors = SlowlyMoreUsefulSingletonForColorUtil.splitRGBToFloats(color);
-
-            // Model handling, tries to reverse the rotation of the shield from the Bow UseAction
-
-            if (mode.equals(ModelTransformation.Mode.FIRST_PERSON_RIGHT_HAND)) {
-                matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(13.935F * 3));
-                matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-35.3F * 2));
-                matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(9.785F * 2));
-            } else {
-                matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(13.935F * 1.5F));
-                matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(35.3F * 1.85F));
-                matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(9.785F * 1));
-            }
-
-            // Create and render model
-            var part = RainbowShieldModel.getTexturedModelData();
-            part.createModel().render(
-                    matrices,
-                    vertexConsumerProvider.getBuffer(RenderLayer.getEnergySwirl(WORLD_BORDER, (float) ((delta * .005f) % 1f), (float) (delta * .005f % 1f))),
-                    light,
-                    OverlayTexture.DEFAULT_UV,
-                    rgbColors[0],
-                    rgbColors[1],
-                    rgbColors[2],
-                    0.5F);
-            matrices.pop();
+            renderRainbowShield(mode, matrices, vertexConsumerProvider, light);
         }
 
         matrices.translate(.5, .5, .5);
-        instance.getItemRenderer().renderItem(staff, mode, false, matrices, vertexConsumerProvider, light, overlay, model);
+        instance.getItemRenderer().renderItem(staff, mode, false, matrices, vertexConsumerProvider, light, overlay, staffModel);
 
-        model.getTransformation().getTransformation(mode).apply(false, matrices);
+        staffModel.getTransformation().getTransformation(mode).apply(false, matrices);
         matrices.scale(0.25F, 0.25F, 0.25F);
         matrices.translate(-.5F, 2F, 0F);
 
         instance.getBlockRenderManager().renderBlockAsEntity(block.getDefaultState(), matrices, vertexConsumerProvider, light, overlay);
-        instance.getBlockRenderManager().getModelRenderer().render(matrices.peek(), ItemRenderer.getItemGlintConsumer(vertexConsumerProvider, RenderLayer.getCutoutMipped(), true, staff.hasGlint()), block.getDefaultState(), blockModel, 0, 0, 0, light, overlay);
+        instance.getBlockRenderManager().getModelRenderer().render(
+                matrices.peek(),
+                ItemRenderer.getItemGlintConsumer(vertexConsumerProvider, RenderLayer.getCutoutMipped(), true, staff.hasGlint()),
+                block.getDefaultState(),
+                blockModel,
+                0,
+                0,
+                0,
+                light,
+                overlay);
     }
 
     @Override
     public void provideExtraModels(ResourceManager manager, Consumer<Identifier> out) {
         out.accept(CARMOT_STAFF_ID);
+    }
+
+    /**
+     * Renders the model of the Stormyx Rainbow Shield, a fancy localized worldborder
+     */
+    private void renderRainbowShield(ModelTransformation.Mode mode, MatrixStack matrices, VertexConsumerProvider vcp, int light) {
+        matrices.push();
+        // Rainbow Handling
+        double delta = System.currentTimeMillis() / 45.0;
+
+        double hue = delta % 360.0;
+        float saturation = 1;
+        float constantvalue = 1;
+
+        int color = MathHelper.hsvToRgb((float) (hue / 360), saturation, constantvalue);
+
+        float[] rgbColors = SlowlyMoreUsefulSingletonForColorUtil.splitRGBToFloats(color);
+
+        // Model handling, tries to reverse the rotation of the shield from the Bow UseAction
+        if (mode.equals(ModelTransformation.Mode.FIRST_PERSON_RIGHT_HAND)) {
+            matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(13.935F * 3));
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-35.3F * 2));
+            matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(9.785F * 2));
+        } else {
+            matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(13.935F * 1.5F));
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(35.3F * 1.85F));
+            matrices.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(9.785F * 1));
+        }
+
+        // Create and render rainbow shield
+        var part = RainbowShieldModel.getTexturedModelData();
+        part.createModel().render(
+                matrices,
+                vcp.getBuffer(RenderLayer.getEnergySwirl(WORLD_BORDER, (float) ((delta * .005f) % 1f), (float) (delta * .005f % 1f))),
+                light,
+                OverlayTexture.DEFAULT_UV,
+                rgbColors[0],
+                rgbColors[1],
+                rgbColors[2],
+                0.5F);
+        matrices.pop();
     }
 }
