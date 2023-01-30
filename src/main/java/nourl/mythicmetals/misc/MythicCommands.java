3df0d739aee685a268882c7f1844d9d59fd4ecdd
Noaaan <noaaan@hotmail.com>
2023-01-30 16:45:30 +0100

    Remove CSV export command
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/nourl/mythicmetals/misc/MythicCommands.java
+++ src/main/java/nourl/mythicmetals/misc/MythicCommands.java
@@ -6,16 +6,12 @@ import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.wispforest.owo.util.ReflectionUtils;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import nourl.mythicmetals.config.MythicOreConfigs;
 import nourl.mythicmetals.config.OreConfig;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.file.Files;
 import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 
@@ -27,64 +23,45 @@ public final class MythicCommands {
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccessdedicated, environment) ->
             dispatcher.register(CommandManager.literal("mythicmetals")
+                .requires(source -> source.hasPermissionLevel(1))
                 .then(CommandManager.literal("range")
                     .then(CommandManager.argument("type", StringArgumentType.word())
                         .suggests(MythicCommands::dumpType)
-                        .requires(source -> source.hasPermissionLevel(1))
                         .executes(context -> {
-                            switch (StringArgumentType.getString(context, "type")) {
-                                case "console" -> {
-                                    ReflectionUtils.iterateAccessibleStaticFields(MythicOreConfigs.class, OreConfig.class, (feature, name, field) -> {
-                                        if (!feature.offset && !feature.trapezoid)
-                                            context.getSource().sendFeedback(Text.literal(
+                            if (StringArgumentType.getString(context, "type").equals("console")) {
+                                ReflectionUtils.iterateAccessibleStaticFields(MythicOreConfigs.class, OreConfig.class, (feature, name, field) -> {
+                                    if (!feature.offset && !feature.trapezoid)
+                                        context.getSource().sendFeedback(Text.literal(
                                                 name.toUpperCase(Locale.ROOT)
-                                                    + " has the range between "
-                                                    + feature.bottom
-                                                    + " to "
-                                                    + feature.top
-                                                    + ", with a discard chance of "
-                                                    + feature.discardChance * 100 + "%"), false);
-                                        if (feature.offset)
-                                            context.getSource().sendFeedback(Text.literal(
+                                                        + " has the range between "
+                                                        + feature.bottom
+                                                        + " to "
+                                                        + feature.top
+                                                        + ", with a discard chance of "
+                                                        + feature.discardChance * 100 + "%"), false);
+                                    if (feature.offset)
+                                        context.getSource().sendFeedback(Text.literal(
                                                 name.toUpperCase(Locale.ROOT)
-                                                    + " has the range between "
-                                                    + feature.bottom
-                                                    + "(offset) to "
-                                                    + feature.top
-                                                    + ", with a discard chance of "
-                                                    + feature.discardChance * 100 + "%"), false);
-                                        if (feature.trapezoid)
-                                            context.getSource().sendFeedback(Text.literal(
+                                                        + " has the range between "
+                                                        + feature.bottom
+                                                        + "(offset) to "
+                                                        + feature.top
+                                                        + ", with a discard chance of "
+                                                        + feature.discardChance * 100 + "%"), false);
+                                    if (feature.trapezoid)
+                                        context.getSource().sendFeedback(Text.literal(
                                                 name.toUpperCase(Locale.ROOT)
-                                                    + " has a triangle range between "
-                                                    + feature.bottom
-                                                    + " to "
-                                                    + feature.top
-                                                    + ", where the sweet spot is at Y = "
-                                                    + ((feature.bottom + feature.top) / 2)
-                                                    + " with a discard chance of "
-                                                    + feature.discardChance * 100 + "%"), false);
+                                                        + " has a triangle range between "
+                                                        + feature.bottom
+                                                        + " to "
+                                                        + feature.top
+                                                        + ", where the sweet spot is at Y = "
+                                                        + ((feature.bottom + feature.top) / 2)
+                                                        + " with a discard chance of "
+                                                        + feature.discardChance * 100 + "%"), false);
 
-                                    });
-                                    return 1;
-                                }
-                                case "file" -> {
-                                    try (var file = Files.newOutputStream(FabricLoader.getInstance().getConfigDir().resolve("debug/mythicmetals.csv"))) {
-                                        var contents = new StringBuilder();
-                                        ReflectionUtils.iterateAccessibleStaticFields(MythicOreConfigs.class, OreConfig.class, (feature, name, field) -> {
-                                            contents.append(name).append(",")
-                                                .append(feature.bottom).append(",")
-                                                .append(feature.top).append(",")
-                                                .append(feature.perChunk).append(",")
-                                                .append(feature.veinSize).append(",")
-                                                .append(feature.offset).append(",")
-                                                .append(feature.trapezoid).append("\n");
-                                    });
-                                    new OutputStreamWriter(file).write(contents.toString());
-                                    return 1;
-                                } catch (IOException e) {
-                                        e.printStackTrace();
-                                }}
+                                });
+                                return 1;
                             }
                             return 1;
 
@@ -96,7 +73,7 @@ public final class MythicCommands {
     }
 
     private static CompletableFuture<Suggestions> dumpType(CommandContext<ServerCommandSource> ctx, SuggestionsBuilder suggestion) {
-        suggestion.suggest("console").suggest("file");
+        suggestion.suggest("console");
         return suggestion.buildFuture();
     }
 }
