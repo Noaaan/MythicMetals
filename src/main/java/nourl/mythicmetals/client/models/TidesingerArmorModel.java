b768561ff584e84a4b9bf6bb004fffe2fcbe464c
Noaaan <noaaan@hotmail.com>
2023-12-02 05:02:04 +0100

    Adjust arm positions on the Tidesinger Armor Model
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/nourl/mythicmetals/client/models/TidesingerArmorModel.java
+++ src/main/java/nourl/mythicmetals/client/models/TidesingerArmorModel.java
@@ -52,19 +52,19 @@ public class TidesingerArmorModel {
 
         ModelPartData right_arm = root.addChild("right_arm", ModelPartBuilder.create(), ModelTransform.pivot(-4.0F, 2.0F, 0.0F));
 
-        var armorRightArm = right_arm.addChild("armorRightArm", ModelPartBuilder.create().uv(41, 52).cuboid(-4.5F, -2.5F, -2.5F, 5.0F, 13.0F, 5.0F)
+        var armor_right_arm = right_arm.addChild("armor_right_arm", ModelPartBuilder.create().uv(41, 52).cuboid(-4.5F, -2.5F, -2.5F, 5.0F, 13.0F, 5.0F)
                 .uv(27, 26).cuboid(-8.5F, 1.5F, -0.5F, 4.0F, 9.0F, 0.0F)
-                .uv(0, 63).cuboid(-5.0F, -4.0F, -3.0F, 6.0F, 7.0F, 6.0F), ModelTransform.NONE);
+                .uv(0, 63).cuboid(-5.0F, -4.0F, -3.0F, 6.0F, 7.0F, 6.0F), ModelTransform.pivot(1.0f, 0.0f, 0.0f));
 
-        var right_arm_fin = armorRightArm.addChild("right_arm_fin", ModelPartBuilder.create().uv(74, 72).cuboid(-4.5F, -6.5F, 0.0F, 5.0F, 13.0F, 0.0F), ModelTransform.of(-5.0F, 4.0F, 0.5F, 0.0F, 0.2618F, 0.0F));
+        var right_arm_fin = armor_right_arm.addChild("right_arm_fin", ModelPartBuilder.create().uv(74, 72).cuboid(-4.5F, -6.5F, 0.0F, 5.0F, 13.0F, 0.0F), ModelTransform.of(-5.0F, 4.0F, 0.5F, 0.0F, 0.2618F, 0.0F));
 
         ModelPartData left_arm = root.addChild("left_arm", ModelPartBuilder.create(), ModelTransform.pivot(4.0F, 2.0F, 0.0F));
 
-        var armorLeftArm = left_arm.addChild("armorLeftArm", ModelPartBuilder.create().uv(21, 51).cuboid(-0.5F, -2.5F, -2.5F, 5.0F, 13.0F, 5.0F)
+        var armor_left_arm = left_arm.addChild("armor_left_arm", ModelPartBuilder.create().uv(21, 51).cuboid(-0.5F, -2.5F, -2.5F, 5.0F, 13.0F, 5.0F)
                 .uv(0, 22).cuboid(4.5F, 1.5F, -0.5F, 4.0F, 9.0F, 0.0F)
-                .uv(61, 59).cuboid(-1.0F, -4.0F, -3.0F, 6.0F, 7.0F, 6.0F), ModelTransform.NONE);
+                .uv(61, 59).cuboid(-1.0F, -4.0F, -3.0F, 6.0F, 7.0F, 6.0F), ModelTransform.pivot(-1.0f, 0.0f, 0.0f));
 
-        var left_arm_fin = armorLeftArm.addChild("left_arm_fin", ModelPartBuilder.create().uv(64, 72).cuboid(-0.5F, -6.5F, 0.0F, 5.0F, 13.0F, 0.0F), ModelTransform.of(5.0F, 4.0F, 0.5F, 0.0F, -0.2618F, 0.0F));
+        var left_arm_fin = armor_left_arm.addChild("left_arm_fin", ModelPartBuilder.create().uv(64, 72).cuboid(-0.5F, -6.5F, 0.0F, 5.0F, 13.0F, 0.0F), ModelTransform.of(5.0F, 4.0F, 0.5F, 0.0F, -0.2618F, 0.0F));
 
         ModelPartData left_leg = root.addChild("left_leg", ModelPartBuilder.create(), ModelTransform.pivot(-2.0F, 12.0F, 0.0F));
 
