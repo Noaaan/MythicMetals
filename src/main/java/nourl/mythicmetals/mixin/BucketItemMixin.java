e1bdc96e7c36d8acb687efad87c2cf32e545cae6
Noaaan <noaaan@hotmail.com>
2025-03-17 20:11:04 +0100

    Fix placement issues related to Palladium Rails
    Since we already correct the blockpos on lavalogged blocks,
    the pos is always correct and can be used directly

--- src/main/java/nourl/mythicmetals/mixin/BucketItemMixin.java
+++ src/main/java/nourl/mythicmetals/mixin/BucketItemMixin.java
@@ -34,14 +34,13 @@ public abstract class BucketItemMixin {
 
     @Inject(method = "placeFluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isLiquid()Z"), cancellable = true)
     private void mythicmetals$fillLavalog(PlayerEntity player, World world, BlockPos pos, BlockHitResult hitResult, CallbackInfoReturnable<Boolean> cir) {
-        var railPos = hitResult == null ? pos : hitResult.getBlockPos();
-        var state = world.getBlockState(railPos);
+        var state = world.getBlockState(pos);
         if (this.fluid.equals(Fluids.LAVA) && state.getBlock() instanceof Lavaloggable lavaloggable) {
             // TODO - Vanilla behavior here is to eat the fluid if you log the same block twice
             // Try and explore whether you can prevent placing lava in the same block twice
             // Lava is mildly more inconvenient to source, after all
-            lavaloggable.tryFillWithFluid(world, railPos, state, Fluids.LAVA.getStill(false));
-            this.playEmptyingSound(player, world, railPos);
+            lavaloggable.tryFillWithFluid(world, pos, state, Fluids.LAVA.getStill(false));
+            this.playEmptyingSound(player, world, pos);
             cir.setReturnValue(true);
         }
     }
