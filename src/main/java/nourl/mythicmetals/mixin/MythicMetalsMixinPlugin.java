0c27ffb0a5d77d01bb08a24d1fd383b2baee2380
Noaaan <noaaan@hotmail.com>
2025-05-18 13:54:58 +0200

    Fix Conduit Mixin not applying outside of dev

--- src/main/java/nourl/mythicmetals/mixin/MythicMetalsMixinPlugin.java
+++ src/main/java/nourl/mythicmetals/mixin/MythicMetalsMixinPlugin.java
@@ -26,9 +26,6 @@ public class MythicMetalsMixinPlugin implements IMixinConfigPlugin {
         if (mixinClassName.equals("nourl.mythicmetals.mixin.PiglinBrainMixin")) {
             return !FabricLoader.getInstance().isModLoaded("custom_piglin_bartering");
         }
-        if (mixinClassName.equals("nourl.mythicmetals.mixin.ConduitBlockEntityMixin")) {
-            return FabricLoader.getInstance().isDevelopmentEnvironment();
-        }
         return true;
     }
 
