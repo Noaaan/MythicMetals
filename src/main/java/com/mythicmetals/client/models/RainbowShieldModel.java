3f0b7762d5682e527d3b2f726290caed4b3c78c1
Noaaan <noaaan@hotmail.com>
2025-05-26 18:14:34 +0200

    Minor Stormyx Shield adjustments

--- src/main/java/com/mythicmetals/client/models/RainbowShieldModel.java
+++ src/main/java/com/mythicmetals/client/models/RainbowShieldModel.java
@@ -9,7 +9,7 @@ public class RainbowShieldModel {
         var root = data.getRoot();
 
         root.addChild("stormyx_shield", ModelPartBuilder.create()
-            .cuboid("shield_cube", 0, 0, 0, 3, 4, 3, new Dilation(16)), ModelTransform.pivot(-0.5f, 13f, -0.5f));
+            .cuboid("shield_cube", 0, 0, 0, 6, 12, 6, new Dilation(8)), ModelTransform.pivot(-2.0f, 12f, -2.0f));
         return TexturedModelData.of(data, 16, 16);
     }
 }
