b8de5ff508cc767fe98ce690f0dfeefdf25ef6af
Noaaan <noaaan@hotmail.com>
2025-12-20 19:35:56 +0100

    Fix Runite Arrows not having increased damage & accuracy

--- src/main/java/com/mythicmetals/mixin/RangedWeaponItemMixin.java
+++ src/main/java/com/mythicmetals/mixin/RangedWeaponItemMixin.java
@@ -1,26 +1,32 @@
 package com.mythicmetals.mixin;
 
+import com.mythicmetals.entity.MythicEntities;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.RangedWeaponItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(RangedWeaponItem.class)
 public abstract class RangedWeaponItemMixin {
 
-    // FIXME
     // Increases the velocity of Runite Arrows when shot from Ranged Weapons
-    // Also decreases divergence, leading to better accuracy
-//    @ModifyArgs(method = "method_61659", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/RangedWeaponItem;shoot(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/projectile/ProjectileEntity;IFFFLnet/minecraft/entity/LivingEntity;)V"))
-//    private void mythicmetals$modifyArrowsForRunite(Args args) {
-//        boolean shouldModify = false;
-//        for (var arrow : projectiles) {
-//            if (arrow.isOf(MythicTools.RUNITE_ARROW) || arrow.isOf(MythicTools.TIPPED_RUNITE_ARROW)) {
-//                shouldModify = true;
-//                break;
-//            }
-//        }
-//        if (shouldModify) {
-//            args.set(3, speed * 1.3f);
-//            args.set(4, divergence * 0.9f);
-//        }
-//    }
+    // Should increase the damage of the arrows noticable
+    // Also decreases divergence, which combined should lead to better accuracy
+    @ModifyArgs(method = "method_61659", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/RangedWeaponItem;shoot(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/projectile/ProjectileEntity;IFFFLnet/minecraft/entity/LivingEntity;)V"))
+    private void mythicmetals$modifyArrowsForRunite(Args args) {
+        boolean shouldModify = false;
+        ProjectileEntity projectile = args.get(1);
+        if (projectile.getType().equals(MythicEntities.RUNITE_ARROW_ENTITY_TYPE)) {
+            shouldModify = true;
+        }
+        if (shouldModify) {
+            float speed = args.get(3);
+            float divergence = args.get(4);
+
+            args.set(3, speed * 1.3f);
+            args.set(4, divergence * 0.85f);
+        }
+    }
 }
