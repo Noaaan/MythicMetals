b6970445d6ad1f4c983c5bcbad7ce396c17e8c3a
Noaaan <noaaan@hotmail.com>
2026-05-27 22:37:55 +0200

    Fix crash with Enchanted Midas Gold Block

--- src/main/java/com/mythicmetals/client/rendering/EnchantedMidasBlockEntityRenderer.java
+++ src/main/java/com/mythicmetals/client/rendering/EnchantedMidasBlockEntityRenderer.java
@@ -2,12 +2,16 @@ package com.mythicmetals.client.rendering;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mythicmetals.block.entity.EnchantedMidasGoldBlockEntity;
+import com.mythicmetals.item.MythicMaterials;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.CameraRenderState;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 
 public class EnchantedMidasBlockEntityRenderer implements BlockEntityRenderer<EnchantedMidasGoldBlockEntity, BlockEntityRenderState> {
     private final BlockRenderDispatcher blockRenderManager;
@@ -18,23 +22,25 @@ public class EnchantedMidasBlockEntityRenderer implements BlockEntityRenderer<En
 
     @Override
     public BlockEntityRenderState createRenderState() {
-        return null;
+        return new BlockEntityRenderState();
     }
 
     @Override
     public void submit(BlockEntityRenderState blockEntityRenderState, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState cameraRenderState) {
-        // FIXME
-//        poseStack.pushPose();
-//        poseStack.translate(-0.001, -0.001, -0.001);
-//        poseStack.scale(1.002f, 1.002f, 1.002f);
-//        blockRenderManager.renderBatched(
-//            MythicBlocks.ENCHANTED_MIDAS_GOLD_BLOCK.defaultBlockState(),
-//            blockEntityRenderState.blockPos,
-//            ,
-//            poseStack,
-//            ItemRenderer.getFoilBuffer(cameraRenderState., RenderTypes.cutoutMovingBlock(), true, true),
-//            true,
-//            List.of());
-//        poseStack.popPose();
+        // FIXME - Z-fighting
+        var blockModel = blockRenderManager.getBlockModel(MythicMaterials.MIDAS_GOLD.blockSet().storage().defaultBlockState());
+        poseStack.pushPose();
+        submitNodeCollector.submitBlockModel(
+            poseStack,
+            RenderTypes.glintTranslucent(),
+            blockModel,
+            1.0f,
+            1.0f,
+            1.0f,
+            blockEntityRenderState.lightCoords,
+            OverlayTexture.NO_OVERLAY,
+            0
+            );
+        poseStack.popPose();
     }
 }
