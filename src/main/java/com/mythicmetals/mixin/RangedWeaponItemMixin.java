8e8002ed2747d732bdf88433797be99076cd2c09
Noaaan <noaaan@hotmail.com>
2026-08-22 14:02:13 +0200

    Fix Runite Arrows not having extra velocity

--- src/main/java/com/mythicmetals/mixin/RangedWeaponItemMixin.java
+++ src/main/java/com/mythicmetals/mixin/RangedWeaponItemMixin.java
@@ -12,22 +12,21 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public abstract class RangedWeaponItemMixin {
 
     // Increases the velocity of Runite Arrows when shot from Ranged Weapons
-    // Should increase the damage of the arrows noticable
+    // Should increase the damage of the arrows noticeably
     // Also decreases divergence, which combined should lead to better accuracy
-    // FIXME
-//    @ModifyArgs(method = "method_61659", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/RangedWeaponItem;shoot(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/projectile/ProjectileEntity;IFFFLnet/minecraft/entity/LivingEntity;)V"))
-//    private void mythicmetals$modifyArrowsForRunite(Args args) {
-//        boolean shouldModify = false;
-//        Projectile projectile = args.get(1);
-//        if (projectile.getType().equals(MythicEntities.RUNITE_ARROW_ENTITY_TYPE)) {
-//            shouldModify = true;
-//        }
-//        if (shouldModify) {
-//            float speed = args.get(3);
-//            float divergence = args.get(4);
-//
-//            args.set(3, speed * 1.3f);
-//            args.set(4, divergence * 0.85f);
-//        }
-//    }
+    @ModifyArgs(method = "lambda$shoot$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ProjectileWeaponItem;shootProjectile(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/projectile/Projectile;IFFFLnet/minecraft/world/entity/LivingEntity;)V"))
+    private void mythicmetals$modifyArrowsForRunite(Args args) {
+        boolean shouldModify = false;
+        Projectile projectile = args.get(1);
+        if (projectile.getType().equals(MythicEntities.RUNITE_ARROW_ENTITY_TYPE)) {
+            shouldModify = true;
+        }
+        if (shouldModify) {
+            float speed = args.get(3);
+            float divergence = args.get(4);
+
+            args.set(3, speed * 1.3f);
+            args.set(4, divergence * 0.85f);
+        }
+    }
 }
