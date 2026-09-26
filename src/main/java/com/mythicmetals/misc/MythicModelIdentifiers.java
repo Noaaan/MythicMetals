c3bf1cf65ef8d1ebbbdca884ff8a68b9fbf332a1
Noaaan <noaaan@hotmail.com>
2026-09-26 12:28:07 +0200

    Fix armor model resource locations
    Also un-removes Banglum

--- src/main/java/com/mythicmetals/misc/MythicModelIdentifiers.java
+++ src/main/java/com/mythicmetals/misc/MythicModelIdentifiers.java
@@ -4,15 +4,11 @@ import net.minecraft.resources.Identifier;
 
 public class MythicModelIdentifiers {
     public static final Identifier ADAMANTITE_ARMOR = RegistryHelper.id("textures/models/armor/adamantite.png");
-    public static final Identifier HALLOWED_CAPE = RegistryHelper.id("textures/models/hallowed_cape.png");
-    public static final Identifier STAR_PLATINUM_CLOAK = RegistryHelper.id("textures/models/star_platinum_cloak.png");
-    public static final Identifier PALLADIUM_MINECART = RegistryHelper.id("textures/models/palladium_minecart.png");
-    public static final Identifier CELESTIUM_ELYTRA = RegistryHelper.id("textures/models/celestium_elytra.png");
-    public static final Identifier HALLOWED_ARMOR = RegistryHelper.id("textures/models/hallowed.png");
-    public static final Identifier LEGENDARY_BANGLUM_ARMOR = RegistryHelper.id("textures/models/banglum.png");
-    public static final Identifier METALLURGIUM_ARMOR = RegistryHelper.id("textures/models/metallurgium.png");
-    public static final Identifier PALLADIUM_ARMOR = RegistryHelper.id("textures/models/palladium.png");
-    public static final Identifier RUNITE_ARMOR = RegistryHelper.id("textures/models/runite.png");
+    public static final Identifier HALLOWED_ARMOR = RegistryHelper.id("textures/models/armor/hallowed.png");
+    public static final Identifier LEGENDARY_BANGLUM_ARMOR = RegistryHelper.id("textures/models/armor/legendary_banglum.png");
+    public static final Identifier METALLURGIUM_ARMOR = RegistryHelper.id("textures/models/armor/metallurgium.png");
+    public static final Identifier PALLADIUM_ARMOR = RegistryHelper.id("textures/models/armor/palladium.png");
+    public static final Identifier RUNITE_ARMOR = RegistryHelper.id("textures/models/armor/runite.png");
 
     public static final Identifier TIDESINGER_BRAIN = RegistryHelper.id("textures/models/armor/tidesinger_brain.png");
     public static final Identifier TIDESINGER_BUBBLE = RegistryHelper.id("textures/models/armor/tidesinger_bubble.png");
@@ -20,4 +16,9 @@ public class MythicModelIdentifiers {
     public static final Identifier TIDESINGER_HORN = RegistryHelper.id("textures/models/armor/tidesinger_horn.png");
     public static final Identifier TIDESINGER_TUBE = RegistryHelper.id("textures/models/armor/tidesinger_tube.png");
     public static final Identifier TIDESINGER = RegistryHelper.id("textures/models/armor/tidesinger.png");
+
+    public static final Identifier HALLOWED_CAPE = RegistryHelper.id("textures/models/hallowed_cape.png");
+    public static final Identifier STAR_PLATINUM_CLOAK = RegistryHelper.id("textures/models/star_platinum_cloak.png");
+    public static final Identifier PALLADIUM_MINECART = RegistryHelper.id("textures/models/palladium_minecart.png");
+    public static final Identifier CELESTIUM_ELYTRA = RegistryHelper.id("textures/models/celestium_elytra.png");
 }
